5b3d78148abf9552d803b80c03049af00b505329
BorntoGO <justregisterid@gmail.com>
2015-08-27 03:05:24 +0800

    Create write.py

---
+++ write.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+__author__ = 'Kyle Yuan'
+
+import serial
+
+def loop():
+    while True:
+        i = raw_input()
+        ser.write(i)
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyAMA0', 9600)
+    try:
+        loop()
+    except KeyboardInterrupt:
+        ser.close()
